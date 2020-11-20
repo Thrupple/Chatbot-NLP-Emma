1f0931dded0f5460b2a03e1bf0fc270d8d51bdd4
Thrupple <108508285+Thrupple@users.noreply.github.com>
2020-11-20 12:44:03 +0000

    fix mem leak

--- misc.py
+++ misc.py
@@ -29,4 +29,4 @@ whWordCodes = ['WDT', 'WP', 'WP$', 'WRB']
 
 trashPOS = ['LS', 'SYM', 'UH', '.', ',', ':', '\"' '(', ')', 'FW']
 
-greetingString
+greetingStrings = [[u'what\'s', u'up'], [u'hi'], [u'yo'], [u'hiy
