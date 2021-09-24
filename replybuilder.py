60118b31d87c511455b4f7e96abb7671846d5882
Thrupple <108508285+Thrupple@users.noreply.github.com>
2021-09-24 17:15:46 +0000

    ASoC: wm8737: Fixup setting VMID Impedance control register

--- replybuilder.py
+++ replybuilder.py
@@ -251,4 +251,6 @@ def make_simple(sentence):
         # We can go one step further
         if random.choice([True, False]) and len(haspropertyAssociations) > 1:
             sentence.contents.append(u'and')
-            sentence.contents.append(weighted_rol
+            sentence.contents.append(weighted_roll(haspropertyAssociations).target)
+        """
+    else
