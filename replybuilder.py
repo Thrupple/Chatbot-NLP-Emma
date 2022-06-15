7bd4082b798387d65632e97cca1956da1c8d5fdb
Thrupple <108508285+Thrupple@users.noreply.github.com>
2022-06-15 09:09:02 +0000

    xen/blkback: correctly respond to unknown, non-native requests

--- replybuilder.py
+++ replybuilder.py
@@ -366,4 +366,5 @@ def reply(message, moodValue, allowInterrogative=True):
             validDomains['compound'] = True
         if moodValue > -0.4:
             # Only allow one interrogative per reply
-            if replyHasInterrogative == F
+            if replyHasInterrogative == False:
+                validDomains['interrogative'] = Tru
