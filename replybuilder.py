7e113fd73a84fcd1e0c6d1169cfb78f4332fa9e6
Thrupple <108508285+Thrupple@users.noreply.github.com>
2022-06-25 12:08:23 +0000

    Save progress

--- replybuilder.py
+++ replybuilder.py
@@ -383,4 +383,4 @@ def reply(message, moodValue, allowInterrogative=True):
         # If we can generate non-interrogative sentences, we would profer to do that
         if len(domains) > 2:
             if 'interrogative' in domains:
- 
+                domains.remove('interrogat
