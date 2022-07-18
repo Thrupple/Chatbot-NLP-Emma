0cacc635aff5c33737f023cf5bb22afceb994b4b
Thrupple <108508285+Thrupple@users.noreply.github.com>
2022-07-18 14:02:20 +0000

    fixing travis build

--- replybuilder.py
+++ replybuilder.py
@@ -395,4 +395,5 @@ def reply(message, moodValue, allowInterrogative=True):
         logging.debug("Chose {0}".format(sentence.domain))
 
         # Build sentence structures
-        logging.info("Building {0} structure for sentence {1} of {2}...".format(sentence.domain, i+1
+        logging.info("Building {0} structure for sentence {1} of {2}...".format(sentence.domain, i+1, len(reply)))
+        if sentence.domain == '
