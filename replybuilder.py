d50d956e1f42b0604d6b93f992671019c4099075
Thrupple <108508285+Thrupple@users.noreply.github.com>
2022-08-31 18:17:52 +0000

    Maven artifacts for GOAL Grammar Tools version \t\t\t\t\t\t2.0.2-SNAPSHOT

--- replybuilder.py
+++ replybuilder.py
@@ -424,4 +424,4 @@ def reply(message, moodValue, allowInterrogative=True):
             reorderedReply.append(sentence)
     reply = reorderedReply
 
-   
+    # Decide whether or not to add a greeting -- var
