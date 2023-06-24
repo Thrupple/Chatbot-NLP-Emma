38f48ff23194e59954bfe0a26707a8bd9df79db4
Thrupple <108508285+Thrupple@users.noreply.github.com>
2023-06-24 20:45:06 +0000

    Modified   ManaPHP/Http/Response.php Modified   ManaPHP/Http/ResponseInterface.php

--- wordpatternfinder.py
+++ wordpatternfinder.py
@@ -7,4 +7,5 @@ class InterrogativePackage:
     Packages the important bits of question nicely
 
     Class variables:
-    questionType    str     Typ
+    questionType    str     Type of question ('what is', 'do X have Y', etc.)
+    attribut
